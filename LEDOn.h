7434b312f1fb446f53122460bb146cf8c528c996
Givingcontrol <95034640+Givingcontrol@users.noreply.github.com>
2022-06-23 07:27:16 +0200

    ESP_ESPNow_Two_way_send_Switch_Receive_Open_Relay
    Send switch closed state.....wait for command to open relay

--- LEDOn.h
+++ LEDOn.h
@@ -4,7 +4,13 @@
 // Must match the sender structure
 typedef struct struct_message {
   bool LedOn = false;
+  bool SwitchClosed = false; // ask if switch closed># >> send switch position
+  bool activateRelay = false; //tell to activate relay
+  bool deactivateRelay = false; // replying relay state
 } struct_message;
 
 // Create a struct_message called myData
-struct_message myData;
+struct_message sentData;
+
+// Create a struct_message to hold incoming data
+struct_message receivedData;
